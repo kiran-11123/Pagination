5d96a1c6b14a398964b3f0016b0dc60ce7c2eee0
Kiran <kiran.sadara6730@gmail.com>
2026-03-31 21:36:05 +0530

    Auth Pages completed

--- frontend/src/componenets/Auth_Pages/Login.tsx
+++ frontend/src/componenets/Auth_Pages/Login.tsx
@@ -1,9 +1,93 @@
+import { useState } from "react"
+import { Link } from "react-router-dom";
+
+export default function Login() {
+
+    const [email, SetEmail] = useState('');
+    const [password, SetPassword] = useState('');
+    const [message, SetMessage] = useState('');
 
 
-export default function Login(){
-    return(
-        <div className="bg-gray-200 h-screen flex items-center justify-center">
-            <h1 className="text-3xl font-bold text-gray-800">Login Page</h1>
+    function SubmitForm() {
+
+
+    }
+
+    return (
+        <div className="flex min-h-screen  flex-col items-center justify-center bg-gray-50 px-4 font-poppins">
+
+            <div className="w-full max-w-md text-sm sm:text-base md:text-lg sm:max-w-lg rounded-xl px-8 shadow-2xl bg-white">
+
+
+                <h1 className="font-bold  text-blue-700 text-center text-lg sm:text-xl mb-6 mt-5">Login Here</h1>
+
+                <form onSubmit={SubmitForm} className="space-y-3">
+
+                    <div>
+
+                        <div>
+                            <label className="font-bold text-md sm:text-lg block mb-1">
+                                Email
+                            </label>
+
+                            <input onChange={(e) => SetEmail(e.target.value)} required value={email} className="w-full px-4 py-2 text-sm rounded-md border-2 border-gray-300 focus:outline-none focus:ring-2 focus:ring-blue-600" placeholder="Enter your Email" type="email" />
+                        </div>
+
+                    </div>
+
+
+                    <div>
+
+                        <div>
+                            <label className="font-bold text-md sm:text-lg  block mb-1">
+                                Password
+                            </label>
+
+                            <input onChange={(e) => SetPassword(e.target.value)} required value={password} className="w-full px-4 py-2 text-sm rounded-md border-2 border-gray-300 focus:outline-none focus:ring-2 focus:ring-blue-600" placeholder="Enter your Password" type="password" />
+                        </div>
+
+                    </div>
+
+
+                    <button className="px-4 py-2 bg-blue-600 text-white  text-sm  text-center rounded-md hover:bg-blue-800 cursor-pointer transition duration-300 mb-3">Submit</button>
+
+
+
+
+                </form>
+
+
+                <div className="w-full mb-5 flex flex-col justify-between items-center text-center " >
+
+                    <p className="text-sm sm:text-lg text-gray-600">
+                        Don’t have an account?{" "}
+                        <Link
+                            to="/register"
+                            className="text-blue-500 hover:underline cursor-pointer"
+                        >
+                            Sign up
+                        </Link>
+                    </p>
+
+                    <p className="text-sm sm:text-md text-gray-600 px-6 py-2 shadow-lg rounded-md">
+                        <Link
+                            to="/forgetpassword"
+                            className="text-blue-500 hover:underline cursor-pointer"
+                        >
+                            Forget password
+                        </Link>
+                    </p>
+
+                </div>
+
+                {message && (
+                    <p className="font-black text-md text-center sm:text-lg mb-5">{message} </p>
+                )}
+
+
+
+            </div>
+
         </div>
     )
 }
--- frontend/src/componenets/Opening_page/home.tsx
+++ frontend/src/componenets/Opening_page/home.tsx
@@ -1,11 +1,11 @@
 import { useState } from "react";
-import Login from "../Auth_Pages/Login";
-import Register from "../Auth_Pages/Register";
 import { useNavigate } from "react-router-dom";
+import { Menu  , X} from "lucide-react";
 
 export default function Home() {
 
      const navigate = useNavigate();
+     const[open ,isOpen] = useState(false);
 
 
      function ToLogin() {
@@ -17,32 +17,58 @@ export default function Home() {
      }
 
      return (
-          <div className="bg-gray-200 h-screen flex flex-col items-center gap-40">
+          <div className="bg-gray-200 h-screen flex flex-col items-center gap-10 justify-start ">
 
 
 
-               <div className="flex  max-w-sm sm:max-w-4xl text-sm sm:text-lg md:text-xl  lg:max-w-full items-center justify-between bg-gradient-to-r from-blue-900 to-violet-800 text-white w-full rounded-lg px-5 py-3 cursor-pointer">
+               <div className="flex  max-w-sm sm:max-w-4xl text-sm sm:text-lg md:text-xl font-bold  lg:max-w-full items-center justify-between bg-gradient-to-r from-blue-900 to-violet-800 text-white w-full rounded-lg px-5 py-3 cursor-pointer">
                     <div className="px-4 py-2 rounded-md border border-white ">
                          NetFly
                     </div>
 
-                    <div className="flex items-center justify-between gap-10 font-roboto">
+                    <div className="hidden sm:flex  items-center justify-between gap-10 font-roboto">
 
-                         <button onClick={ToLogin} className="px-4 py-2 cursor-pointer hover:bg-gradient-to-tr from-white/20 via-white/5 to-transparent  rounded-md  shadow-md">
+                         <button onClick={ToLogin} className="px-4 py-2 cursor-pointer hover:bg-gradient-to-tr from-white/20 via-white/30 to-transparent  rounded-md  shadow-md">
                               Login
                          </button>
 
-                         <button onClick={ToRegister} className="px-4 py-2 cursor-pointer hover:bg-gradient-to-tr from-white/20 via-white/5 to-transparent  rounded-md  shadow-md">
+                         <button onClick={ToRegister} className="px-4 py-2 cursor-pointer hover:bg-gradient-to-tr from-white/20 via-white/30 to-transparent  rounded-md  shadow-md">
                               Register
                          </button>
 
                     </div>
 
+                    <div className="sm:hidden flex items-center">
+                         <button onClick={()=>isOpen(!open)} className="text-white focus:outline-none">
+                              {open ? <X className="h-6 w-6" /> : <Menu className="h-6 w-6" />}
+                         </button>
+                    </div>
 
+
+                 
 
                </div>
 
-               <div className="flex flex-col items-center text-center bg-white/20 backdrop-blur-md border border-white/20 px-8 py-6 rounded-2xl shadow-lg max-w-lg sm:max-w-xl md:max-w-2xl">
+                
+
+
+
+               <div className="flex flex-col items-center font-poppins gap-6 justify-center w-full px-4">
+                    
+                      {open && (
+                <div className="sm:hidden w-full max-w-xl px-6 mt-2 flex flex-col gap-2 bg-[#0f0c29]  text-white rounded-xl shadow-lg text-center text-sm py-4">
+                    <button  onClick={ToLogin} className="px-4 py-2 rounded-lg bg-white/10 hover:bg-white/20 transition font-medium">
+                        Login
+                    </button>
+                    <button onClick={ToRegister} className="px-4 py-2 rounded-lg bg-purple-600 hover:bg-purple-700 transition font-medium">
+                        Register
+                    </button>
+                </div>
+            )}
+
+             <div className="flex flex-col text-center bg-white/20 backdrop-blur-md border border-white/20 px-8 py-6 rounded-2xl shadow-lg max-w-lg sm:max-w-xl md:max-w-2xl ">
+
+            
 
                     <h2 className="text-sm sm:text-base font-bold mb-3">
                          NetFly – Stream Your World 
@@ -54,6 +80,9 @@ export default function Home() {
                     </p>
 
                </div>
+
+
+           </div>
 
 
 
