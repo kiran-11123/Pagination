3fdedcf107e3215102d3eb8df6ea2c6a0dea4413
Kiran <kiran.sadara6730@gmail.com>
2026-04-02 20:22:32 +0530

    Added products into Database

--- frontend/src/componenets/Home/HomePage.tsx
+++ frontend/src/componenets/Home/HomePage.tsx
@@ -1,5 +1,46 @@
+import axios from "axios";
+import { useState  , useEffect } from "react"
+
+const Products_API = import.meta.env.VITE_ORDERS_API
 
 export default function HomePage(){
+   
+     const[products , SetProducts] = useState([]);
+     const[message , SetMessage] = useState('');
+    
+
+
+    useEffect(()=>{
+
+         async function getProducts(){
+               
+            try{
+
+                const response = await axios.get(`${Products_API}/products/get-products` ,{
+                    withCredentials : true
+                } )
+
+                if(response.status === 200){
+                      SetProducts(response.data.products);
+                      SetMessage('products fetched successfully')
+                }
+                else{
+                     
+                    SetMessage(response.data.message);
+                }
+
+            }
+            catch(er){
+                 SetMessage("Something went wrong while fetching the products")
+            }
+            
+         }
+
+         getProducts();
+
+    } ,[])
+
+   
       
     return (
          <div >
