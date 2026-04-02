f23d72e89ba8be067a82353cd62d08cb985c273a
Kiran <kiran.sadara6730@gmail.com>
2026-04-02 19:31:06 +0530

    kafka added

---
+++ frontend/src/componenets/Home/NavBar.tsx
@@ -0,0 +1,10 @@
+
+
+export default function NavBar(){
+      
+    return(
+         <div className="flex items-center justify-between px-5 py-2 rounded-lg  ">
+             
+         </div>
+    )
+}
