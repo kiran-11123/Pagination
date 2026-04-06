5af7adb5c40ae791a2ec549683b3d9cf640253bc
Kiran <kiran.sadara6730@gmail.com>
2026-04-06 22:07:28 +0530

    Prisma is not working in orders-backend

--- order_service_backend/app/controller/DataController.js
+++ order_service_backend/app/controller/DataController.js
@@ -1,15 +1,27 @@
 
+import { GetDataService } from "../service/DataService.js"
 
-
-export const GetDataController = (req,res)=>{
+export const GetDataController = async (req,res)=>{
        
     try{
           
-        
+          
+
+        const data  = await GetDataService();
+
+        return res.status(200).json({
+              message : "Data fetched successfully",
+              products : data
+        })
          
 
     }
     catch(er){
+
+
+          return res.status(500).json({
+             message : "Internal server Error"
+          })
           
     }
 }
