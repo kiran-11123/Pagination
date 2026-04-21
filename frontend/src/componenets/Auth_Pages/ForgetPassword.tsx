960174379e9c524971302d19bef68ea84b2ad0ab
Kiran <kiran.sadara6730@gmail.com>
2026-04-21 20:11:32 +0530

    Reset Password is working

--- frontend/src/componenets/Auth_Pages/ForgetPassword.tsx
+++ frontend/src/componenets/Auth_Pages/ForgetPassword.tsx
@@ -111,6 +111,9 @@ export default function ForgetPassword() {
              setTimeout(()=>{
                  
                 SetMessage('');
+                SetEmail('')
+                SetNewPassword('')
+                SetOTP('');
              } , 2000)
         }
           
