19144d5a8f67eb1a25951fc113fd668091f8d52e
Kiran <kiran.sadara6730@gmail.com>
2026-04-23 14:05:42 +0530

    Done with removeCart API

--- Cart_backend/app/routes/RemoveCartRoutes.js
+++ Cart_backend/app/routes/RemoveCartRoutes.js
@@ -2,8 +2,12 @@
 
 import express from 'express'
 const Remove_Cart_Router = express.Router();
+import { Access_token_Middleware } from '../../../backend/app/middlewares/AccessToken_middleware.js';
+
+import { RemoveCartController } from '../controller/RemoveCartController.js';
 
 
+Remove_Cart_Router.delete('/remove_cart_item' , Access_token_Middleware, RemoveCartController);
 
 
 
